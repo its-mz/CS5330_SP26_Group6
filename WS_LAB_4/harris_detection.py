81c04c42dfca95e48c909d68a209a31b899b24e1
its-mz <ou.mi@northeastern.edu>
2026-02-03 12:37:26 -0800

    Workshop 3 output upload

--- WS_LAB_4/harris_detection.py
+++ WS_LAB_4/harris_detection.py
@@ -16,13 +16,14 @@ dst = cv2.cornerHarris(gray, 2, 3, 0.04)
 # Dilate to make corners visible
 dst = cv2.dilate(dst, None)
 
+# Threshold for marking corners in red
+img[dst > 0.01 * dst.max()] = [0, 0, 255]
+
 # Save the output image 
 output_dir = "output"
 output_path = os.path.join(output_dir, 'harris_corners_output.jpg')
 cv2.imwrite(output_path, img)
 
-# Threshold for marking corners in red
-img[dst > 0.01 * dst.max()] = [0, 0, 255]
 cv2.imshow('Workshop 1 - Harris Corners', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
